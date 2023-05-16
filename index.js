6ff700779061c12a13d3dc3dc610f44b7348fc1d
biroue isaac <biroueisaac@gmail.com>
2023-05-16 22:03:27 +0100

    Add cors module to my project

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const morgan = require('morgan')
-app.use(morgan('tiny'));
+app.use(morgan('tiny'))
 app.use(express.json())
 let persons = [
     {
